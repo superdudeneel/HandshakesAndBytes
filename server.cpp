c379ded05f55c44b79d75987dcd769a26fa6db4f
Neel Sane <neelsane@gmail.com>
2025-05-31 10:59:13 +0530

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -1,74 +1,91 @@
 #include <iostream>
 #include <winsock2.h>
 #include <ws2tcpip.h>
-
 using namespace std;
 #pragma comment(lib, "ws2_32.lib")
 
 bool fn_dll() {
-    WSADATA data;
+    WSAData data;
     return WSAStartup(MAKEWORD(2, 2), &data) == 0;
+
 }
 
 int main() {
-    cout << "server side code" << endl;
-
     if (!fn_dll()) {
-        cout << "failed to initialize the dll" << endl;
-    } else {
-        cout << "DLL success" << endl;
+        cout << "DLL failed to iniliaze" << endl;
+        return 1;
+    }
+    else {
+        cout << "DLL initialized" << endl;
+
     }
 
     SOCKET listensocket = socket(AF_INET, SOCK_STREAM, 0);
     if (listensocket == INVALID_SOCKET) {
-        cout << "failed to create socket" << endl;
-    } else {
-        cout << "socket success" << endl;
+        cout << "SOCKET creation failed" << endl;
+        return 1;
+    }
+    else {
+        cout << "SOCKET created" << endl;
+
     }
 
     sockaddr_in server_address;
-    server_address.sin_family = AF_INET;
+    server_address.sin_family  = AF_INET;
     server_address.sin_port = htons(1234);
-
-    if (InetPton(AF_INET, "127.0.0.1", &server_address.sin_addr) != 1) {
-        cout << "address structure failed" << endl;
+    if (InetPton(AF_INET, "127.0.0.1", &server_address.sin_addr.s_addr)!=1) {
+        cout << "Server address not valid" << endl;
         closesocket(listensocket);
         WSACleanup();
         return 1;
     }
+    else {
+        cout << "Server structure valid" << endl;
 
+    }
     if (bind(listensocket, (sockaddr*)&server_address, sizeof(server_address)) == SOCKET_ERROR) {
-        cout << "bind failed" << endl;
-    } else {
-        cout << "bind success" << endl;
-    }
-
-    if (listen(listensocket, SOMAXCONN) == SOCKET_ERROR) {
-        cout << "listen failed" << endl;
+        cout << "Server bind failed" << endl;
         closesocket(listensocket);
         WSACleanup();
         return 1;
-    } else {
-        cout << "listen success" << endl;
+    }
+    else {
+        cout << "Server bind ready" << endl;
     }
 
-    cout << "Server is up and running" << endl;
+    if (listen(listensocket, SOMAXCONN)!=SOCKET_ERROR) {
+        cout << "Server listening" << endl;
+    }
+    else {
+        cout << "Server listening failed" << endl;
+        closesocket(listensocket);
+        WSACleanup();
+        return 1;
+    }
 
-    SOCKET clientsocket = accept(listensocket, nullptr, nullptr);
+    SOCKET clientsocket  = accept(listensocket, NULL, NULL);
     if (clientsocket == INVALID_SOCKET) {
-        cout << "accept failed" << endl;
-    } else {
-        cout << "accept success" << endl;
+        cout << "Accept failed" << endl;
+    }
+    else {
+        cout << "Accept ready" << endl;
     }
 
-    char buffer[4096];
-    int bytes_received = recv(clientsocket, buffer, sizeof(buffer), 0);
-    string message(buffer, bytes_received);
-    cout << "message from client: " << message << endl;
+    while (true) {
+        char buffer[4096];
+        int bytes_received = recv(clientsocket, buffer, sizeof(buffer), 0);
+        string message(buffer, bytes_received);
+        if (message == "quit") {
+            closesocket(clientsocket);
+            closesocket(listensocket);
+            break;
 
-    closesocket(clientsocket);
-    closesocket(listensocket);
+        }
+        cout << "message from client: " << message << endl;
+
+    }
+
+
+
     WSACleanup();
-
-    return 0;
 }
